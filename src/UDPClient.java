d8d04a99d9aef3b040400763ffa1ff31725a0424
Jonathan Forbes <jsebrof@gmail.com>
2014-04-18 00:26:09 -0700

    Basically Done, just need to add timestamp to client code and make both "robust to malformed packets"

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -5,22 +5,44 @@ class UDPClient
 	public static void main(String args[]) throws Exception
 	{
 		System.out.println("Client Start");
+		
 		String serverIP = args[0];
 		int port = Integer.parseInt(args[1]);
-		String sentence = args[2];
-		System.out.println("The IP is: " + serverIP + " port: " + port + " msg " + sentence);
+		String operation = args[2];
+		String key = args[3];
+		String value = "";
+		if (operation.toLowerCase().matches("put"))
+		{
+			value = args[4];
+		}
+		
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName(serverIP); // n01
+		
 		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
-		sendData = sentence.getBytes();
+		sendData = operation.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+		System.out.println("Sending packets to the server");
 		clientSocket.send(sendPacket);
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		System.out.println("Sending message to the server");
-		clientSocket.receive(receivePacket);
-		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER:" + modifiedSentence);
+		sendData = key.getBytes();
+		sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+		clientSocket.send(sendPacket);
+		if (operation.toLowerCase().matches("put"))
+		{
+			sendData = value.getBytes();
+			sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+			clientSocket.send(sendPacket);
+		}
+		
+		if (operation.toLowerCase().matches("get"))
+		{
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + modifiedSentence.trim());
+		}
+						
 		clientSocket.close();
 		System.out.println("Client Close");
 	}
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,26 +1,71 @@
 import java.net.*;
+import java.util.HashMap;
 
 class UDPServer
 {
 	public static void main(String args[]) throws Exception
 	{
-		System.out.println("Server Start");
-		DatagramSocket serverSocket = new DatagramSocket(9833); // just kill the server to release the socket
-		byte[] receiveData = new byte[1024];
+		long timestart = System.currentTimeMillis();
+		HashMap<String, String> store = new HashMap<String, String>();
+		int port = Integer.parseInt(args[0]);
+		System.out.println("Server Start at " + (System.currentTimeMillis()-timestart));
+		DatagramSocket serverSocket = new DatagramSocket(port); // just kill the server to release the socket
+		
+		byte[] receiveOperationData = new byte[1024];
+		byte[] receiveKeyData = new byte[1024];
+		byte[] receiveValueData = new byte[1024];
 		byte[] sendData = new byte[1024];
+		
 		while(true)
 		{
-			System.out.println("Waiting to receive message...");
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			serverSocket.receive(receivePacket);
-			String sentence = new String( receivePacket.getData());
-			System.out.println("RECEIVED: " + sentence);
-			InetAddress IPAddress = receivePacket.getAddress();
-			int port = receivePacket.getPort();
-			String capitalizedSentence = sentence.toUpperCase();
-			sendData = capitalizedSentence.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
+			System.out.println("Waiting to receive message at " + (System.currentTimeMillis()-timestart));
+			DatagramPacket receiveOperationPacket = new DatagramPacket(receiveOperationData, receiveOperationData.length);
+			DatagramPacket receiveKeyPacket = new DatagramPacket(receiveKeyData, receiveKeyData.length);
+			DatagramPacket receiveValuePacket = new DatagramPacket(receiveValueData, receiveValueData.length);
+			
+			serverSocket.receive(receiveOperationPacket);
+			serverSocket.receive(receiveKeyPacket);
+			
+			String operation = new String(receiveOperationPacket.getData());
+			operation = operation.trim().toLowerCase();
+			
+			if (operation.matches("put"))
+			{
+				serverSocket.receive(receiveValuePacket);
+			}
+			
+			String key = new String(receiveKeyPacket.getData());
+			key = key.trim();
+			String value = new String(receiveValuePacket.getData());
+			value = value.trim();
+			InetAddress IPAddress = receiveOperationPacket.getAddress();
+			int clientPort = receiveOperationPacket.getPort();
+			String returnMessage = "";
+			System.out.println("Request from: " + IPAddress + ":" + clientPort + " for " + operation + " " + key + " " + value + " at " + (System.currentTimeMillis()-timestart));
+			
+			
+			switch (operation)
+			{
+			case "put":
+				store.put(key, value);
+				System.out.println("Key \"" + key + "\" Value \"" + store.get(key) + "\" stored at " + (System.currentTimeMillis()-timestart));
+				break;
+			case "get":
+				System.out.println("Value \"" + store.get(key) + "\" for Key \"" + key + "\" returned to " + IPAddress + ":" + clientPort + " at " + (System.currentTimeMillis()-timestart));
+				returnMessage = store.get(key);
+				break;
+			case "delete":
+				store.remove(key);
+				System.out.println("Key \"" + key + "\" Value \"" + store.get(key) + "\" deleted at " + (System.currentTimeMillis()-timestart));
+				break;
+			}
+
+			if (operation.matches("get"))
+			{
+				sendData = returnMessage.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, clientPort);
+				serverSocket.send(sendPacket);
+			}
 		}
 	}
 }
